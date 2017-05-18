1780d524436208e3b95fa7757add9cc4f14638f3
Bartek <gondek.inpg@gmail.com>
2017-05-18 23:07:01 +0200

    poprawki w klasach

--- Klasa punkt+klasa snake.cpp
+++ Klasa punkt+klasa snake.cpp
@@ -2,75 +2,88 @@
 
 using namespace std;
 
-class Point
+class Point //podstawowa klasa opisuj¹ca punkt x,y w ukl wsp
 {
-    int x,y;
-    Point (int xx, int yy)
+    int width, height;
+
+    public:
+
+    Point (int w, int h)
     {
-        x=xx;
-        y=yy;
+        width=w;
+        height=h;//zgodnie z konwencja narzucona przez michala pierwsza wsp odpowiada wysokosci a druga szerokosci
     }
 
-    public
-
-    void MoveTo (int xx, int yy)
+    void MoveTo (int w, int h)
     {
-        x=xx;
-        y=yy;
+        width=w;
+        height=h;
     }
-    bool IsEqual (int xx, int yy)
+
+    bool IsEqual (int w, int h) //metoda sprawdzaj¹ca czy punkt ma podane wspolrzedne
     {
-        if(xx==x&&yy==y) return true;
+        if(w==width&&h==height) return true;
         else return false;
+    }
+
+    int widthOut()
+    {
+        return width;
+    }
+    int heightOut()
+    {
+        return height;
     }
 };
 
-class Snake
+class Snake //klasa opisuj¹ca polozenie/ulozenie weza w tablicy
 {
-    Point** places;
+    Point** places; //tablica (dynamicznie alokowana bo ilosc segmentow bedzie sie zwiekszac) zawieraj¹ca punkty w ktorych znajduja sie kolejne segmenty weza
     int l;
 
-    public
+    public:
 
-    Snake(int xx, int yy, char dir='w'/* zmienic na enum!*/, int ll=3)
+    Snake(int w, int h, Kierunek=RIGHT, int ll=3)
     {
         l=ll;
         places=new Point*[l];
-        places[0]=new Point(xx,yy)
-            switch(dir)
-            {
-            case n:
-                for(int i=1;i<l;i++)
-                {
-                    places[i]=new Point(xx,yy+i)
-                }
-            case s:
-                for(int i=1;i<l;i++)
-                {
-                    places[i]=new Point(xx,yy-i)
-                }
-            case w:
-                for(int i=1;i<l;i++)
-                {
-                    places[i]=new Point(xx+,yy)
-                }
-            case e:
-                for(int i=1;i<l;i++)
-                {
-                    places[i]=new Point(xx-i,yy-i)
-                }
-            }
+        switch(dir)
+        {
+        case UP:
+            for(int i=0;i<l;i++) places[i]=new Point(w+i,h);
+            break;
 
+        case DOWN:
+            for(int i=1;i<l;i++) places[i]=new Point(w-i,h);
+            break;
+
+        case LEFT:
+            for(int i=1;i<l;i++) places[i]=new Point(w,h+i);
+            break;
+
+        case RIGHT:
+            for(int i=1;i<l;i++) places[i]=new Point(w,h-i);
+            break;
         }
+
     }
 
-    ~Snake
+
+    ~Snake()
+    {
+        for(int i=0;i<l;i++) delete places[i];
+        delete places;
+    }
+
+    int howLong ()
+    {
+        return l;
+    }
+
+    void rysuj (char **tab)
     {
         for(int i=0;i<l;i++)
-        {
-            delete places(i);
-        }
-        delete places;
+            tab[][]// do dokonczenia
     }
 };
 
