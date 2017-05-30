4b70b003afa834af2d101c7cc765d8329efd206f
DESKTOP-GM7EJVG\Michas <michalkrzyszczk@gmail.com>
2017-05-30 09:28:01 +0200

    Usunięcie niepotrzebnych funkcji wywoływanych w main.

--- SKOŃCZONY SNAKE SFML/main.cpp
+++ SKOŃCZONY SNAKE SFML/main.cpp
@@ -383,26 +383,20 @@ int main()
     char nazwa[50];
     char** tab;
     int poziom;
-
-
     FILE*plikoutpkt=fopen("danewyjpkt.txt","a");
     FILE*plikoutnames=fopen("danewyjnames.txt","a");
-
-    cout<<"Witaj w terminalowej grze snake"<<endl;
+    cout<<"Witaj w grze snake"<<endl;
     cout<<"podaj nazwe gracza"<<endl;
     fgets(nazwa,50,stdin);
     fprintf(plikoutnames,"%s",nazwa);
     fclose(plikoutnames);
     cout<<"Podaj szerokosc planszy"<<endl;
     cin>>szer;
-
     cout<<"Podaj wysokosc planszy"<<endl;
     cin>>wys;
     cout<<"Podaj poziom trudnosci w skali 1-10"<<endl;
     cin>>poziom;
     poziom=200/poziom;
-
-
     sf::ConvexShape gwiazda(5);
     gwiazda.setPoint(0,sf::Vector2f(1,6));
     gwiazda.setPoint(1,sf::Vector2f(19,6));
@@ -410,23 +404,17 @@ int main()
     gwiazda.setPoint(3,sf::Vector2f(10,0));
     gwiazda.setPoint(4,sf::Vector2f(16,18));
     gwiazda.setFillColor(sf::Color::Yellow);
-
     tab=stworz(wys,szer);//piszemy w konwencji tab[wysokosc][szerekosc] czyli tab[2][5] to bedzie element w 3 wierwszy i 6 kolumnie*/
     tab=ramka(tab,wys,szer);
-
     generuj_jedzenie(tab,wys,szer,rfh,rfw);
     gwiazda.setPosition(fw*20,fh*20);
-
-
     sf::RenderWindow window(sf::VideoMode(szer*20, wys*20), "WOLOLOLO");
     sf::RenderWindow& pwindow=window;
-
     sf::RectangleShape plansza(sf::Vector2f((szer-2)*20,(wys-2)*20));
     plansza.setPosition(border_left,border_up);
     plansza.setFillColor(sf::Color(150,150,150));
     plansza.setOutlineColor(sf::Color::Red);
     plansza.setOutlineThickness(20);
-
     sf::ConvexShape convex(6);
     convex.setPoint(0, sf::Vector2f(5, 0));
     convex.setPoint(1, sf::Vector2f(15, 0));
@@ -437,25 +425,18 @@ int main()
     convex.setPosition(100,100);
     convex.setFillColor(sf::Color::Green);
     convex.rotate(90);
-
     sf::CircleShape* kola;
     kola=new sf::CircleShape[3];
     kola[0].setPosition(100,100);
     kola[0].setRadius(30);
-
     kola[1].setPosition(100,200);
     kola[1].setRadius(30);
-
     kola[2].setPosition(200,200);
     kola[2].setRadius(30);
-
-
     sf::ContextSettings settings;
     settings.antialiasingLevel = 8;
-
     cout<<endl;
     system("pause");
-
     while (window.isOpen())
     {
         sf::Event event;
@@ -496,10 +477,6 @@ int main()
                     break;
                 }
             }
-
-
-        system("cls");
-
         if(snake.crash(tab,dir)==true)
             {fprintf(plikoutpkt,"%i\n",punkty);// MOZESZ PISAC plikout<<punkty (jak do couta)
             fclose(plikoutpkt);
@@ -514,17 +491,11 @@ int main()
             punkty++;
         }
         else snake.step(dir); // wykonuje krok
-        snake.draw(tab); // wpisuje weza po wykonaniu kroku do tablicy
-        system("cls");
-        wyswietl(tab,wys,szer); //wypisuje tablice na ekran
-
-
         window.clear();
         window.draw(plansza);
         window.draw(gwiazda);
-
-
-        for(int i=0;i<snake.howLong()-1;i++)window.draw(snake.tail[i]);
+        for(int i=0;i<snake.howLong()-1;i++)
+            window.draw(snake.tail[i]);
         window.draw(snake.glowa);
         window.display();
         Sleep(poziom);
